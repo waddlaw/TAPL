6a0771965d8265d9c7304c5ef1843c61ed01d146
Shinya Yamaguchi <ingronze@gmail.com>
2018-06-07 19:36:29 +0900

    Update

--- src/Language/NB.hs
+++ src/Language/NB.hs
@@ -14,19 +14,18 @@ eval t =
     Right t' -> eval t'
 
 eval1 :: Term -> Either TmError Term
-eval1 (TmIf TmTrue  t2 _t3)   = Right t2
-eval1 (TmIf TmFalse _t2 t3)   = Right t3
-eval1 (TmIf t1 t2 t3)         = TmIf <$> eval1 t1 <*> pure t2 <*> pure t3
-eval1 (TmSucc t1)             = TmSucc <$> eval1 t1
-eval1 (TmPred TmZero)         = Right TmZero
+eval1 (TmIf TmTrue  t2 _t3)                        = Right t2
+eval1 (TmIf TmFalse _t2 t3)                        = Right t3
+eval1 (TmIf t1 t2 t3)                              = TmIf   <$> eval1 t1 <*> pure t2 <*> pure t3
+eval1 (TmSucc t1)                                  = TmSucc <$> eval1 t1
+eval1 (TmPred TmZero)                              = Right TmZero
 eval1 (TmPred (TmSucc nv1))
-  | isNumericalVal nv1        = Right nv1
-eval1 (TmPred t1)             = TmPred <$> eval1 t1
-eval1 (TmIsZero TmZero)       = Right TmTrue
-eval1 (TmIsZero (TmSucc nv1))
-  | isNumericalVal nv1        = Right TmFalse
-eval1 (TmIsZero t1)           = TmIsZero <$> eval1 t1
-eval1 _                       = Left NoRuleApplies
+  | isNumericalVal nv1                             = Right nv1
+eval1 (TmPred t1)                                  = TmPred <$> eval1 t1
+eval1 (TmIsZero TmZero)                            = Right TmTrue
+eval1 (TmIsZero (TmSucc (isNumericalVal -> True))) = Right TmFalse
+eval1 (TmIsZero t1)                                = TmIsZero <$> eval1 t1
+eval1 _                                            = Left NoRuleApplies
 
 isNumericalVal :: Term -> Bool
 isNumericalVal TmZero      = True

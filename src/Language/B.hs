ec6cb756c0468dd19336773231369a176baf9e2e
waddlaw <ingronze@gmail.com>
2018-06-08 11:31:15 +0900

    GHC の warning 直し

--- src/Language/B.hs
+++ src/Language/B.hs
@@ -4,8 +4,6 @@ module Language.B
   ( deduce
   , step
   , reduction
-  , true
-  , false
   ) where
 
 import           Language.B.Types
@@ -14,30 +12,24 @@ import           Data.Maybe
 
 -- | 項の評価関係
 --
--- 評価関係は t -> t' と書かれ、「tが1ステップでt'に評価される」と読む
+-- 評価関係 t -> t' は「tが1ステップでt'に評価される」と読む
 reduction :: Maybe Premise -> Term -> Term
-reduction Nothing (TmIf TmTrue  t2 t3) = t2                 -- E-IFTRUE
-reduction Nothing (TmIf TmFalse t2 t3) = t3                 -- E-IFFALSE
+reduction Nothing (TmIf TmTrue  t2 _t3) = t2                -- E-IFTRUE
+reduction Nothing (TmIf TmFalse _t2 t3) = t3                -- E-IFFALSE
 reduction (Just premise) (TmIf t1 t2 t3) = TmIf t1' t2 t3   -- E-IF
   where
     t1' = fromMaybe (error msg) $ lookup t1 [unwrap premise]
     msg = show t1 ++ " -> ???"
+reduction _ _ = error "No Rule"
 
 deduce :: Rule -> Conclusion -> Maybe Premise
-deduce E_IFTRUE (EvalRelation (t, t')) =
-  let TmIf TmTrue t2 t3 = t
-  in  if t2 == t' then Nothing else error "unify mismatch"
-deduce E_IFFALSE (EvalRelation (t, t')) =
-  let TmIf TmFalse t2 t3 = t
-  in  if t3 == t' then Nothing else error "unify mismatch"
-deduce E_IF (EvalRelation (t, t')) =
-  let TmIf t1 t2 t3 = t
-      TmIf t1' t2' t3' = t'
-  in  if t2 == t2' && t3 == t3' then Just (EvalRelation (t1, t1')) else error "unify mismatch"
+deduce E_IFTRUE (EvalRelation ((TmIf TmTrue t2 _t3), t')) =
+  if t2 == t' then Nothing else error "unify mismatch"
+deduce E_IFFALSE (EvalRelation ((TmIf TmFalse _t2 t3), t')) =
+  if t3 == t' then Nothing else error "unify mismatch"
+deduce E_IF (EvalRelation ((TmIf t1 t2 t3 ), (TmIf t1' t2' t3'))) =
+  if t2 == t2' && t3 == t3' then Just (EvalRelation (t1, t1')) else error "unify mismatch"
+deduce _ _ = error "unify mismatch"
 
 step :: Rule -> Maybe Premise -> Maybe Premise
-step rule = maybe Nothing (deduce rule)
-
-true, false :: Term
-true  = TmTrue
-false = TmFalse
+step rule mpremise = mpremise >>= deduce rule
--- test/Test/Language/B.hs
+++ test/Test/Language/B.hs
@@ -9,22 +9,18 @@ import           Language.B.Types
 test_reduction :: TestTree
 test_reduction = testGroup "簡約チェック"
   [ testCase "reduction" $ do
-      let true  = TmTrue
-          false = TmFalse
-          t1    = TmIf false false false
-          t     = TmIf true t1 true
+      let t1 = TmIf TmFalse TmTrue TmFalse
+          t  = TmIf TmTrue t1 TmTrue
       reduction Nothing t @?= t1
   ]
 
 test_proof :: TestTree
 test_proof = testGroup "証明"
   [ testCase "導出可能" $ do
-      let true  = TmTrue
-          false = TmFalse
-          s = TmIf true false false
-          t = TmIf s true true
-          u = TmIf false true true
-          z1 = EvalRelation (TmIf t false false, TmIf u false false)
+      let s  = TmIf TmTrue TmFalse TmFalse
+          t  = TmIf s TmTrue TmTrue
+          u  = TmIf TmFalse TmTrue TmTrue
+          z1 = EvalRelation (TmIf t TmFalse TmFalse, TmIf u TmFalse TmFalse)
           z2 = EvalRelation (TmIf TmTrue TmTrue TmFalse, TmTrue)
           z3 = EvalRelation (TmIf TmFalse TmTrue TmFalse, TmFalse)
 

6ced0a341f7028b69a558546c30d91dd95ceaeb5
waddlaw <ingronze@gmail.com>
2018-06-09 11:56:24 +0900

    IsString のインスタンス追加

--- src/Language/UntypedLambda/Types.hs
+++ src/Language/UntypedLambda/Types.hs
@@ -2,7 +2,9 @@ module Language.UntypedLambda.Types
   ( Term (..)
   ) where
 
+import           Data.String
 import           Data.Text                 (Text)
+import qualified Data.Text                 as T
 import           Data.Text.Prettyprint.Doc
 
 data Term
@@ -15,3 +17,6 @@ instance Pretty Term where
   pretty (TmVar x)     = pretty x
   pretty (TmLam x t)   = pretty "λ" <> pretty x <> pretty "." <+> pretty t
   pretty (TmApp t1 t2) = pretty t1 <+> pretty t2
+
+instance IsString Term where
+  fromString = TmVar . T.pack

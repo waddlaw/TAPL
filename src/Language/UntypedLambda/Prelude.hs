67f69093913dedb7ba4e536d428dd9401e0084eb
waddlaw <ingronze@gmail.com>
2018-06-19 12:33:20 +0900

    Church ブール値 tru, fls ,test 追加

--- src/Language/UntypedLambda/Prelude.hs
+++ src/Language/UntypedLambda/Prelude.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Language.UntypedLambda.Prelude
   ( id
+  -- Church ブール値
+  , tru
+  , fls
+  , test
   ) where
 
 import           Prelude                hiding (id)
@@ -10,3 +14,15 @@ import           Language.UntypedLambda
 -- | λx. x
 id :: Term
 id = TmLam "x" "x"
+
+-- | λt. λf. t
+tru :: Term
+tru = TmLam "t" (TmLam "f" "t")
+
+-- | λt. λf. f
+fls :: Term
+fls = TmLam "t" (TmLam "f" "f")
+
+-- | λl. λm. λn. l m n
+test :: Term
+test = TmLam "l" (TmLam "m" (TmLam "n" (TmApp (TmApp "l" "m") "n")))

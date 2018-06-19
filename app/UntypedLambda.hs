5a69c437a937a4624b116dc612c231d9e4752bf0
waddlaw <ingronze@gmail.com>
2018-06-19 20:04:59 +0900

    trace モード追加

--- app/UntypedLambda.hs
+++ app/UntypedLambda.hs
@@ -4,27 +4,62 @@ import           Language.UntypedLambda
 import           Language.UntypedLambda.Prelude (prelude)
 import           Language.Utils
 
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.State.Strict
 import           System.Console.Haskeline
+
+type UntypedLambda = InputT (StateT Env IO) ()
+
+data Env = Env
+  { strategy :: Strategy
+  , isTrace :: Bool
+  }
+
+defaultEnvironment :: Env
+defaultEnvironment = Env NormalOrder False
 
 main :: IO ()
 main = do
   putStrLn "Start untyped lambnda repl"
 
-  _ <- runInputT defaultSettings main'
+  _ <- runStateT (runInputT defaultSettings main') defaultEnvironment
 
   putStrLn "Leaving untyped lambda repl"
 
-main' :: InputT IO ()
+main' :: UntypedLambda
 main' = do
   minput <- getInputLine "UntypedLambda> "
   case trim <$> minput of
     Nothing      -> return ()
     Just ":q"    -> return ()
     Just ":list" -> listCmd >> main'
+    Just ":trace" -> traceCmd >> main'
     Just input   -> evalCmd input >> main'
 
-listCmd :: InputT IO ()
+traceCmd :: UntypedLambda
+traceCmd = do
+  env <- getEnv
+  putEnv $ env { isTrace = not $ isTrace env }
+
+listCmd :: UntypedLambda
 listCmd = outputStrLn $ renderPrelude prelude
 
-evalCmd :: String -> InputT IO ()
-evalCmd = outputStrLn . either id (render . eval NormalOrder) . runUlParser
+evalCmd :: String -> UntypedLambda
+evalCmd input = do
+  istrace <- isTrace <$> getEnv
+
+  case runUlParser input of
+    Left err -> outputStrLn err
+    Right term ->
+      if istrace then
+        do
+          _ <- lift $ lift $ trace NormalOrder term
+          return ()
+      else
+        outputStrLn $ render $ eval NormalOrder term
+
+getEnv :: InputT (StateT a IO) a
+getEnv = lift get
+
+putEnv :: s -> InputT (StateT s IO) ()
+putEnv = lift . put

91da77234d807c6bc404b7ed2bb221d8e1038ebf
waddlaw <ingronze@gmail.com>
2018-06-20 06:15:12 +0900

    help コマンドも実装

--- app/UntypedLambda.hs
+++ app/UntypedLambda.hs
@@ -23,6 +23,7 @@ defaultEnvironment = Env NormalOrder False
 main :: IO ()
 main = do
   putStrLn "Start untyped lambnda repl"
+  putStrLn ":help でコマンドの一覧が確認できます。"
 
   _ <- runStateT (runInputT defaultSettings main') defaultEnvironment
 
@@ -41,8 +42,24 @@ main' = do
          | ":list strategy" `isPrefixOf` input -> listStrategyCmd            >> main'
          | ":list prelude"  `isPrefixOf` input -> listPreludeCmd             >> main'
          | ":env"           `isPrefixOf` input -> printEnvCmd                >> main'
+         | ":help"          `isPrefixOf` input -> helpCmd                    >> main'
          -- main process
          | otherwise -> evalCmd input >> main'
+
+helpCmd :: UntypedLambda
+helpCmd = mapM_ outputStrLn $ "available commands" : commands
+
+commands :: [String]
+commands =
+  [ "  :set trace               -- トレースの有効化 (簡約の途中経過も含めて表示)"
+  , "  :set strategy <Strategy> -- 評価戦略の設定"
+  , "  :unset trace             -- トレースの無効化"
+  , "  :list strategy           -- 評価戦略の一覧を表示"
+  , "  :list prelude            -- prelude 関数の一覧を表示"
+  , "  :env                     -- 現在の設定内容を表示"
+  , "  :help                    -- ヘルプ"
+  , "  :q                       -- 終了"
+  ]
 
 printEnvCmd :: UntypedLambda
 printEnvCmd = do

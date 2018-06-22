1e27827c143264f931871c7931dc4d5b87d32786
waddlaw <ingronze@gmail.com>
2018-06-22 17:52:02 +0900

    リファクタリング

--- src/Language/Extensible/UntypedLambda/Types.hs
+++ src/Language/Extensible/UntypedLambda/Types.hs
@@ -3,8 +3,9 @@ module Language.Extensible.UntypedLambda.Types
   , Strategy (..)
   ) where
 
-import           Data.Extensible
+import Language.UntypedLambda.Types (Strategy (..))
 
+import           Data.Extensible
 import Control.Lens    (( # ))
 
 import           Data.String
@@ -12,11 +13,13 @@ import           Data.Text (Text)
 import qualified Data.Text                 as T
 import           Data.Text.Prettyprint.Doc
 
-newtype Term = Term { unwrapTerm :: ( Variant
-  '[ "var"    >: Text
-   , "lambda" >: (Text, Term)
-   , "app"    >: (Term, Term)
-   ])}
+newtype Term = Term
+  { unwrapTerm :: Variant
+      '[ "var"    >: Text
+      , "lambda" >: (Text, Term)
+      , "app"    >: (Term, Term)
+      ]
+   }
 
 instance Pretty Term where
   pretty = matchField prettyTerm . unwrapTerm
@@ -36,10 +39,3 @@ ppr = matchField pm . unwrapTerm
 
 instance IsString Term where
   fromString v = Term $ #var # T.pack v
-
-data Strategy
-  = FullBetaReduction -- ^ 完全ベータ簡約
-  | NormalOrder       -- ^ 正規順序戦略
-  | CallByName        -- ^ 名前呼び戦略
-  | CallByValue       -- ^ 値呼び戦略
-  deriving (Show, Read, Enum, Bounded)

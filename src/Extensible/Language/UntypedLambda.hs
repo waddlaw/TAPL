bd399050767d7a3c4a0fb013e95d3b34006ef8cd
waddlaw <ingronze@gmail.com>
2018-06-24 19:11:59 +0900

    パーサーモジュールを re-export

--- src/Extensible/Language/UntypedLambda.hs
+++ src/Extensible/Language/UntypedLambda.hs
@@ -1,5 +1,7 @@
 module Extensible.Language.UntypedLambda
   ( module Extensible.Language.UntypedLambda.Types
+  , module Extensible.Language.UntypedLambda.Parser
   ) where
 
 import           Extensible.Language.UntypedLambda.Types
+import           Extensible.Language.UntypedLambda.Parser

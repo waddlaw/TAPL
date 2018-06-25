2b3e2371b54c5fe54b810b4b120790a887690a52
waddlaw <ingronze@gmail.com>
2018-06-25 14:04:56 +0900

    freevar が壊れてる

--- src/Extensible/Language/UntypedLambda.hs
+++ src/Extensible/Language/UntypedLambda.hs
@@ -5,3 +5,116 @@ module Extensible.Language.UntypedLambda
 
 import           Extensible.Language.UntypedLambda.Types
 import           Extensible.Language.UntypedLambda.Parser
+import           Language.Utils
+
+import Data.Extensible
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Text (Text)
+
+-- | 指定された評価戦略で項を正規系に評価する
+eval :: Strategy -> Term -> Term
+eval s t
+  | result == t = t
+  | otherwise = eval s result
+  where
+    result = evalOneStep s t
+
+-- | デバッグ用
+trace :: Strategy -> Term -> IO ()
+trace s t = mapM_ (putStrLn . render) $ reverse $ evalWithTrace s [t] t
+
+-- | 簡約ステップ列を返す
+evalWithTrace :: Strategy -> [Term] -> Term -> [Term]
+evalWithTrace s acc t
+  | result == t = acc
+  | otherwise = evalWithTrace s acc' result
+  where
+    result = evalOneStep s t
+    acc'   = result:acc
+
+-- | 簡約ステップ数を返す
+steps :: Term -> Int
+steps = length . evalWithTrace NormalOrder []
+
+-- | 1ステップのみ、指定された評価戦略で評価する
+evalOneStep :: Strategy -> Term -> Term
+evalOneStep FullBetaReduction _ = undefined -- TODO
+evalOneStep NormalOrder       t = reduceNormalOrder t
+evalOneStep CallByName        t = reduceCallByName t
+evalOneStep CallByValue       t = reduceCallByValue t
+
+-- | 正規順序戦略
+reduceNormalOrder :: Term -> Term
+reduceNormalOrder = undefined
+-- reduceNormalOrder (TmApp (TmLam x old) new)             = subst x new old
+-- reduceNormalOrder (TmApp t1@(TmApp _ _) t2@(TmApp _ _)) = TmApp (reduceNormalOrder t1) (reduceNormalOrder t2)
+-- reduceNormalOrder (TmApp t1@(TmApp _ _) t2)             = TmApp (reduceNormalOrder t1) t2
+-- reduceNormalOrder (TmApp t1 t2@(TmApp _ _))             = TmApp t1 (reduceNormalOrder t2)
+-- reduceNormalOrder (TmLam v t)                           = TmLam v (reduceNormalOrder t)
+-- reduceNormalOrder t                                     = t
+
+-- | 名前呼び戦略
+reduceCallByName :: Term -> Term
+reduceCallByName = undefined
+-- reduceCallByName (TmApp (TmLam x old) new)             = subst x new old
+-- reduceCallByName (TmApp t1@(TmApp _ _) t2@(TmApp _ _)) = TmApp (reduceCallByName t1) (reduceCallByName t2)
+-- reduceCallByName (TmApp t1@(TmApp _ _) t2)             = TmApp (reduceCallByName t1) t2
+-- reduceCallByName (TmApp t1 t2@(TmApp _ _))             = TmApp t1 (reduceCallByName t2)
+-- reduceCallByName t                                     = t
+
+-- | 値呼び戦略
+reduceCallByValue :: Term -> Term
+reduceCallByValue = undefined
+-- reduceCallByValue (TmApp t@(TmLam x old) new)
+--   | isValue new = subst x new old
+--   | otherwise   = TmApp t (reduceCallByValue new)
+-- reduceCallByValue (TmApp t1@(TmApp _ _) t2@(TmApp _ _)) = TmApp (reduceCallByValue t1) (reduceCallByValue t2)
+-- reduceCallByValue (TmApp t1@(TmApp _ _) t2) = TmApp (reduceCallByValue t1) t2
+-- reduceCallByValue (TmApp t1 t2@(TmApp _ _)) = TmApp t1 (reduceCallByValue t2)
+-- reduceCallByValue t = t
+
+-- | β-reduction
+subst :: Text -> Term -> Term -> Term
+subst = undefined
+-- subst v1 new t@(TmVar v2)
+--   | v1 == v2  = new
+--   | otherwise = t
+-- subst v1 new t@(TmLam v2 t')
+--   | v1 == v2  = t
+--   | otherwise = TmLam v2 (subst v1 new t')
+-- subst v new (TmApp t1 t2) = TmApp t1' t2'
+--   where
+--     t1' = subst v new t1
+--     t2' = subst v new t2
+
+-- | 与えられた項が閉じているかどうか判定する述語
+isClosed :: Term -> Bool
+isClosed = Set.null . freeVars Set.empty
+
+-- | 項に含まれる自由変数を返す
+freeVars :: Set Text -> Term -> Set Text
+freeVars fv = matchField pm . unwrapTerm
+  where
+    pm = #var    @= (\v -> if Set.member v fv then Set.empty else Set.singleton v)
+      <: #lambda @= (\v t -> freeVars (Set.insert v fv) t)
+      <: #app    @= (Set.union <$> freeVars fv <*> freeVars fv)
+      <: nil
+
+-- freeVars fv (TmLam v t) = freeVars fv' t
+--   where
+--     fv' = Set.insert v fv
+-- freeVars fv (TmApp t1 t2) = fv1 `Set.union` fv2
+--   where
+--     fv1 = freeVars fv t1
+--     fv2 = freeVars fv t2
+
+-- | 与えられた項が値かどうか判定する述語
+isValue :: Term -> Bool
+isValue = matchField pm . unwrapTerm
+  where
+    pm = #var    @= const True
+      <: #lambda @= const True
+      <: #app    @= const False
+      <: nil

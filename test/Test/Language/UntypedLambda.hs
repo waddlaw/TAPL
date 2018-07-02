c6668ac5e48dc839151fed54f3ebd6b9ca3021c4
waddlaw <ingronze@gmail.com>
2018-07-02 13:12:22 +0900

    動くところは教科書に合わせて CallByValue に変更

--- test/Test/Language/UntypedLambda.hs
+++ test/Test/Language/UntypedLambda.hs
@@ -196,12 +196,12 @@ test_ul = testGroup "UntypedLambda"
       eval NormalOrder (TmApp tail (TmLam "c" (TmLam "n" (TmApp (TmApp "c" "x") "n")))) @?= nil
       eval NormalOrder (TmApp tail (TmLam "c" (TmLam "n" (TmApp (TmApp "c" "x") (TmApp (TmApp "c" "y") "n"))))) @?= TmLam "c" (TmLam "n" (TmApp (TmApp "c" "y") "n"))
   , testCase "convert NB <-> church" $ do
-      eval NormalOrder (TmApp realbool fls) @?= TmVar "false"
-      eval NormalOrder (TmApp realbool tru) @?= TmVar "true"
-      eval NormalOrder (TmApp (TmApp realeq (c 0)) (c 0)) @?= TmVar "true"
-      eval NormalOrder (TmApp (TmApp realeq (c 0)) (c 1)) @?= TmVar "false"
+      eval CallByValue (TmApp realbool fls) @?= TmVar "false"
+      eval CallByValue (TmApp realbool tru) @?= TmVar "true"
+      eval CallByValue (TmApp (TmApp realeq (c 0)) (c 0)) @?= TmVar "true"
+      eval CallByValue (TmApp (TmApp realeq (c 0)) (c 1)) @?= TmVar "false"
       eval NormalOrder (TmApp realnat (c 2)) @?= TmApp (TmVar "succ") (TmApp (TmVar "succ") (TmVar "0"))
 
-      eval NormalOrder (TmApp (TmApp equal (c 4)) (TmApp (TmApp times (c 2)) (c 2))) @?= tru
+      eval CallByValue (TmApp (TmApp equal (c 4)) (TmApp (TmApp times (c 2)) (c 2))) @?= tru
       eval NormalOrder (TmApp realnat (TmApp (TmApp times (c 2)) (c 2))) @?= TmApp (TmVar "succ") (TmApp (TmVar "succ") (TmApp (TmVar "succ") (TmApp (TmVar "succ") (TmVar "0"))))
   ]

8d16696d6554b2a4608f30bc72b166d98e6ee2de
waddlaw <ingronze@gmail.com>
2018-07-03 15:22:23 +0900

    Int のテストコードをスマートコンストラクタに変更

--- test/Test/Language/UntypedLambda/Lib/Int.hs
+++ test/Test/Language/UntypedLambda/Lib/Int.hs
@@ -10,16 +10,16 @@ import           Language.UntypedLambda.Prelude
 test_ul :: TestTree
 test_ul = testGroup "UntypedLambda.Lib.Int"
   [ testCase "int" $ do
-      eval CallByValue (int 0)    @?= eval CallByValue (TmApp (TmApp pair tru) (c 0))
-      eval CallByValue (int (-3)) @?= eval CallByValue (TmApp (TmApp pair fls) (c 3))
-      eval CallByValue (int 3)    @?= eval CallByValue (TmApp (TmApp pair tru) (c 3))
+      eval CallByValue (int 0)    @?= eval CallByValue (mkPair tru (c 0))
+      eval CallByValue (int (-3)) @?= eval CallByValue (mkPair fls (c 3))
+      eval CallByValue (int 3)    @?= eval CallByValue (mkPair tru (c 3))
   , testCase "succI" $
-      eval CallByValue (TmApp succI (int 0)) @?= eval CallByValue (int 1)
+      eval CallByValue (succI @@ (int 0)) @?= eval CallByValue (int 1)
       -- 振る舞い等価
       -- eval CallByValue (TmApp succI (int (-1))) @?= eval CallByValue (int 0)
       -- eval CallByValue (TmApp succI (int 1))    @?= eval CallByValue (int 2)
   , testCase "succNI" $
-      eval CallByValue (TmApp succNI (TmApp (TmApp pair (c 1)) (int 0))) @?= eval CallByValue (int 1)
+      eval CallByValue (succNI @@ mkPair (c 1) (int 0)) @?= eval CallByValue (int 1)
       -- 振る舞い等価
       -- eval CallByValue (TmApp (TmApp plusI (int 1)) (int 1)) @?= eval CallByValue (int 2)
   ]

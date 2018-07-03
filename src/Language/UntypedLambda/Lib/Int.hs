b15602c11ea3cf9a0937e03dc66f2ea4588ca5ef
waddlaw <ingronze@gmail.com>
2018-07-03 15:14:16 +0900

    Int モジュールをスマートコンストラクタを使っ書き直し

--- src/Language/UntypedLambda/Lib/Int.hs
+++ src/Language/UntypedLambda/Lib/Int.hs
@@ -18,49 +18,52 @@ import           Language.UntypedLambda.Types
 -- +0 = (True, 0)
 int :: Int -> UntypedLambda
 int n
-  | n < 0     = TmApp (TmApp pair fls) (c $ abs n)
-  | otherwise = TmApp (TmApp pair tru) (c n)
+  | n < 0     = mkPair fls (c $ abs n)
+  | otherwise = mkPair tru (c n)
 
 plusI :: UntypedLambda
-plusI = TmLam "i1" $ TmLam "i2" $ TmApp (TmApp (TmApp test isPP) pp) t2
+plusI = λ "n" $ λ "m" $ mkTest isPP pp $ mkTest isNN nn' t3
   where
-    isPP = TmApp (TmApp and c1l) c1r
-    c1l  = TmApp fst "i1"
-    c1r  = TmApp fst "i2"
     -- positive + positive
-    pp = TmApp succNI (TmApp (TmApp pair (TmApp snd "i1")) "i2")
-    t2 = TmApp (TmApp (TmApp test isNN) nn') t3
-    isNN = TmApp (TmApp and c2l) c2r
-    c2l  = TmApp not (TmApp fst "i1")
-    c2r  = TmApp not (TmApp fst "i2")
-    -- negative + negative
-    nn'  = TmApp (TmApp pair fls) (TmApp snd (TmApp succNI (TmApp (TmApp pair (TmApp snd "i1")) (TmApp (TmApp pair tru) (TmApp snd "i2")))))
-    -- cmp 作るの疲れたから left: positive, right: negative とする
-    t3 = TmApp succNI (TmApp (TmApp pair (TmApp snd "i1")) "i2")
+    isPP = and @@ (fst @@ "n") @@ (fst @@ "m")
+    pp   = succNI @@ mkPair (snd @@ "n") "m"
 
+    -- negative + negative
+    isNN = and @@ (not @@ (fst @@ "n")) @@ (not @@ (fst @@ "m"))
+    nn'  = mkPair fls $ snd @@ (succNI @@ mkPair (snd @@ "n") (mkPair tru (snd @@ "m")))
+    -- cmp 作るの疲れたから left: positive, right: negative とする
+    t3 = succNI @@ mkPair (snd @@ "n") "m"
+
+-- | λf. λp. test (iszro (fst p)) (λx. snd p) (λx. succI (f (pair (prd (fst p)) (snd p)))) c0
 succNI :: UntypedLambda
-succNI = TmApp fix ff
+succNI = fix @@ ff
   where
-    ff = TmLam "f" $ TmLam "p" $ TmApp (TmApp (TmApp (TmApp test (TmApp iszro (TmApp fst "p"))) (TmLam "x" $ TmApp snd "p")) t) (c 0)
-    t  = TmLam "x" $ TmApp succI (TmApp "f" (TmApp (TmApp pair (TmApp prd (TmApp fst "p"))) (TmApp snd "p")))
+    ff = λ "f" $ λ "p" $ mkTest match base rec @@ c 0
+    match = iszro @@ (fst @@ "p")
+    base = λ "x" $ snd @@ "p"
+    rec  = λ "x" $ succI @@ ("f" @@ mkPair (prd @@ (fst @@ "p")) (snd @@ "p"))
 
 -- | if isZero i
 --   then (True, 1)
 --   else if isNegative i
 --        then (fst i, prd (snd i))
 --        else (fst i, scc (snd i))
+--
+-- λi. test (isZeroI i) (pair tru c1) (test (isPositiveI i) (pair (fst i) (scc (snd i))) (pair (fst i) (prd (snd i))))
 succI :: UntypedLambda
-succI = TmLam "i" $ TmApp (TmApp (TmApp test c1) t1) t2
+succI = λ "i" $ mkTest c1 t1 $ mkTest c2 t2 t3
   where
-    c1 = TmApp isZeroI "i"
-    t1 = TmApp (TmApp pair tru) (c 1)
-    t2 = TmApp (TmApp (TmApp test c2) t3) t4
-    c2 = TmApp isPositiveI "i"
-    t3 = TmApp (TmApp pair (TmApp fst "i")) (TmApp scc (TmApp snd "i"))
-    t4 = TmApp (TmApp pair (TmApp fst "i")) (TmApp prd (TmApp snd "i"))
+    c1 = isZeroI @@ "i"
+    c2 = isPositiveI @@ "i"
+    t1 = mkPair tru (c 1)
+    t2 = mkPair (fst @@ "i") (scc @@ (snd @@ "i"))
+    t3 = mkPair (fst @@ "i") (prd @@ (snd @@ "i"))
 
+-- | λi. iszro (snd i)
 isZeroI :: UntypedLambda
-isZeroI = TmLam "i" $ TmApp iszro (TmApp snd "i")
+isZeroI = λ "i" $ iszro @@ (snd @@ "i")
 
+-- | λi. fst i
 isPositiveI :: UntypedLambda
-isPositiveI = TmLam "i" $ TmApp fst "i"
+isPositiveI = λ "i" $ fst @@ "i"
+

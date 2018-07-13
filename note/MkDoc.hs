1a15b13cdd4ccac9876f257006fc527ddc08a0b3
waddlaw <ingronze@gmail.com>
2018-07-13 16:43:25 +0900

    とりあえず叩き台のスクリプト作成

---
+++ note/MkDoc.hs
@@ -0,0 +1,67 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-12.0
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import qualified Data.Text as T
+import qualified Data.Text.IO                as T
+import qualified Data.Text.Lazy.IO           as TL
+import Lucid
+import qualified Text.MMark                  as MMark
+import qualified Text.MMark.Extension.Common as Ext
+import Text.MMark.Extension (Extension, Inline (..), Block (..))
+import qualified Text.MMark.Extension as Ext
+import Control.Monad
+
+main :: IO ()
+main = do
+  let input = "ch03.md"
+  txt <- T.readFile input
+  case MMark.parse input txt of
+    Left errs -> putStrLn (MMark.parseErrorsPretty txt errs)
+    Right r ->
+      let toc = MMark.runScanner r (Ext.tocScanner (> 1))
+      in  TL.writeFile "output.html"
+          . renderText
+          . wrapper
+          . MMark.render
+          . MMark.useExtensions
+            [ Ext.toc "toc" toc
+            , Ext.punctuationPrettifier
+            , Ext.ghcSyntaxHighlighter
+            , Ext.skylighting
+            , Ext.mathJax (Just '$')
+            , mathJaxBlock
+            ]
+          $ r
+
+wrapper :: Html () -> Html ()
+wrapper content = do
+  doctype_
+  html_ $ do
+    head_ $ do
+      meta_ [ charset_ "utf-8"]
+      meta_ [ name_ "viewport", content_ "width=device-width, initial-scale=1" ]
+      title_ [] "TAPL"
+      link_  [rel_ "stylesheet", href_ "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"]
+      link_  [rel_ "stylesheet", href_ "./hilight.css"]
+      link_  [rel_ "stylesheet", href_ "./base.css"]
+      script_ [ defer_ T.empty , src_ "https://use.fontawesome.com/releases/v5.1.0/js/all.js" ] T.empty
+      script_ [ async_ T.empty, src_ "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-AMS_CHTML" ] T.empty
+    body_ $
+      section_ [class_ "section"] $
+        div_ [class_ "container"] $
+          div_ [class_ "content"] $ do
+            content
+
+mathJaxBlock :: Extension
+mathJaxBlock = Ext.blockRender $ \old block ->
+  case block of
+    b@(CodeBlock mlabel txt) ->
+      if mlabel == Just "mathjaxBlock"
+        then do
+               p_ . forM_ (T.lines txt) $ \x -> toHtml x
+               "\n"
+        else old b
+    other -> old other

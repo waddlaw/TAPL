5119738e50da43163292609bee07c9d75fe2b2a5
waddlaw <ingronze@gmail.com>
2018-09-21 12:03:04 +0900

    SimpleLambda のテストケースを追加

---
+++ test/Test/Language/SimpleLambda.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Test.Language.SimpleLambda where
+
+import RIO
+
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+import           Language.SimpleLambda
+
+test_sl :: TestTree
+test_sl = testGroup "SimpleLambda"
+  [ testCase "pretty" $ do
+      prettySimpleText ["x"] (TmVar 0) @?= "x"
+      prettySimpleText [] (TmLam "x" TyBool (TmVar 0)) @?= "λx:Bool. x"
+      prettySimpleText ["b"] (TmApp (TmLam "b" TyBool (TmVar 0)) TmTrue) @?= "(λb:Bool. b) true"
+      prettySimpleText [] (TmIf TmTrue TmFalse TmFalse) @?= "if true then false else false"
+  ]

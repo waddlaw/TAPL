63f27ec88220b8e819a7facbc68af95c9ed1f7a1
Restyled.io <commits@restyled.io>
2018-11-24 05:44:34 +0000

    Restyled

--- subs/lambda-fullsimple/src/Language/FullSimpleLambda.hs
+++ subs/lambda-fullsimple/src/Language/FullSimpleLambda.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns      #-}
 module Language.FullSimpleLambda
   ( module Language.FullSimpleLambda.Types
   , module Language.FullSimpleLambda.Parser
@@ -17,15 +17,15 @@ import           Language.FullSimpleLambda.Pretty
 import           Language.FullSimpleLambda.Types
 
 eval :: Term -> Term
-eval (TmIf TmTrue t2 t3) = t2                                    -- E-IFTRUE
-eval (TmIf TmFalse t2 t3) = t3                                   -- E-IFFALSE
-eval (TmIf t1@(isValue -> False) t2 t3) = TmIf (eval t1) t2 t3   -- E-IF
-eval (TmApp (TmLam x _ t1) v2@(isValue -> True)) = subst x v2 t1 -- E-APPABS
+eval (TmIf TmTrue t2 t3)                             = t2                                    -- E-IFTRUE
+eval (TmIf TmFalse t2 t3)                            = t3                                   -- E-IFFALSE
+eval (TmIf t1@(isValue -> False) t2 t3)              = TmIf (eval t1) t2 t3   -- E-IF
+eval (TmApp (TmLam x _ t1) v2@(isValue -> True))     = subst x v2 t1 -- E-APPABS
 eval (TmApp (TmWildcard _ t12) t2@(isValue -> True)) = t12       -- E-WILDCARD
-eval (TmApp t1@(isValue -> False) t2) = TmApp (eval t1) t2       -- E-APP1
-eval (TmApp v1@(isValue -> True) t2) = TmApp v1 (eval t2)        -- E-APP2
-eval (TmSeq t1@(isValue -> False) t2) = TmSeq (eval t1) t2       -- E-SEQ
-eval (TmSeq t1@(isValue -> True) t2) = t2                        -- E-SEQNEXT
+eval (TmApp t1@(isValue -> False) t2)                = TmApp (eval t1) t2       -- E-APP1
+eval (TmApp v1@(isValue -> True) t2)                 = TmApp v1 (eval t2)        -- E-APP2
+eval (TmSeq t1@(isValue -> False) t2)                = TmSeq (eval t1) t2       -- E-SEQ
+eval (TmSeq t1@(isValue -> True) t2)                 = t2                        -- E-SEQNEXT
 
 subst :: Text -> Value -> Term -> Term
 subst = undefined
@@ -62,12 +62,12 @@ typeof ctx (TmWildcard tyT1 t2) = TyArr tyT1 (typeof ctx t2)  -- T-WILDCARD
 -- | 対象の構文
 --
 -- TmSeq
--- 
+--
 -- TmWildcard
 desugar :: Term -> Term
-desugar (TmSeq t1 t2) = TmApp (TmLam "x" TyUnit t2) t1 -- FIXME x notin FV(t2)
+desugar (TmSeq t1 t2)     = TmApp (TmLam "x" TyUnit t2) t1 -- FIXME x notin FV(t2)
 desugar (TmWildcard ty t) = TmLam "x" ty t -- FIXME x notin FV(t)
-desugar term = term
+desugar term              = term
 
 ----------------------
 -- helper functions --

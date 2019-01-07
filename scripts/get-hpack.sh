73e72cc317dd94264674eb8636c35933b0aa2e27
waddlaw <ingronze@gmail.com>
2019-01-07 15:53:43 +0900

    Update

--- scripts/get-hpack.sh
+++ scripts/get-hpack.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 
 PACKAGE=hpack
+VERSION=0.31.1
 echo "Downloading $PACKAGE now ..."
 
 RELEASES=$(curl --silent https://github.com/sol/$PACKAGE/releases)
 URL=https://github.com/$(echo "$RELEASES" | grep -o '\"[^\"]*_linux\.gz\"' | sed s/\"//g | head -n1)
-VERSION=$(echo "$URL" | sed -e 's/.*-\([\.0-9]\+\)_linux\.gz/\1/')
 
 curl --progress-bar --location -o"$PACKAGE.gz" "$URL"
 gzip -d "$PACKAGE.gz"

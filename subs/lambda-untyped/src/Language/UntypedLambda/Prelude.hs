f859392c37dce686e2fc098e145f79456cc17472
waddlaw <ingronze@gmail.com>
2019-05-22 14:38:31 +0900

    either と isLeft, isRight を定義した。

--- subs/lambda-untyped/src/Language/UntypedLambda/Prelude.hs
+++ subs/lambda-untyped/src/Language/UntypedLambda/Prelude.hs
@@ -4,16 +4,16 @@ module Language.UntypedLambda.Prelude
   ( prelude
   ) where
 
-import           RIO                               hiding (and, fst, id, not,
-                                                    or, snd)
-import qualified RIO.Map                           as Map
+import RIO hiding (and, fst, id, not, or, snd, isLeft)
+import qualified RIO.Map as Map
 
-import           Language.UntypedLambda.Lib.Base
-import           Language.UntypedLambda.Lib.Bool
-import           Language.UntypedLambda.Lib.Church
-import           Language.UntypedLambda.Lib.List
-import           Language.UntypedLambda.Lib.Pair
-import           Language.UntypedLambda.Types
+import Language.UntypedLambda.Lib.Base
+import Language.UntypedLambda.Lib.Bool
+import Language.UntypedLambda.Lib.Church
+import Language.UntypedLambda.Lib.Either
+import Language.UntypedLambda.Lib.List
+import Language.UntypedLambda.Lib.Pair
+import Language.UntypedLambda.Types
 
 
 prelude :: Map Text UntypedLambda
@@ -21,5 +21,6 @@ prelude = Map.fromList
   [ ("id", id), ("tru", tru), ("fls", fls), ("test", test), ("and", and), ("or", or), ("not", not)
   , ("pair", pair), ("fst", fst), ("snd", snd)
   , ("scc", scc), ("plus", plus), ("times", times), ("power", power1), ("iszro", iszro), ("prd", prd), ("subtract", subtract1), ("equal", equal)
+  , ("left", left), ("right", right), ("isLeft", isLeft)
   , ("nil", nil), ("cons", cons), ("isnil", isnil), ("head", head), ("tail", tail)
   ]
